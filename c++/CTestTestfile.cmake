fea92c787e685782ccf16e7432afac4241d21253
dave <dmh2000@gmail.com>
2024-12-02 11:27:29 -0800

    added c++ version with google test

---
+++ c++/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/dmh2000/projects/mermaid-states/c++
+# Build directory: /home/dmh2000/projects/mermaid-states/c++
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(state_machine_test "/home/dmh2000/projects/mermaid-states/c++/state_machine_test")
+set_tests_properties(state_machine_test PROPERTIES  _BACKTRACE_TRIPLES "/home/dmh2000/projects/mermaid-states/c++/CMakeLists.txt;25;add_test;/home/dmh2000/projects/mermaid-states/c++/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
