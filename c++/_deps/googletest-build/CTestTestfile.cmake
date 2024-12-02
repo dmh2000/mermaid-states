6766c79c6d28555bc9b46ce0a49e76e46333146c
dave <dmh2000@gmail.com>
2024-12-02 11:47:31 -0800

    deleted google test deps

--- c++/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmh2000/projects/mermaid-states/c++/_deps/googletest-src
-# Build directory: /home/dmh2000/projects/mermaid-states/c++/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- c++/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmh2000/projects/mermaid-states/c++/_deps/googletest-src/googlemock
-# Build directory: /home/dmh2000/projects/mermaid-states/c++/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
